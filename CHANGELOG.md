628f10b4a580e689f4ff243085f501b42388a24d
Matheus Evangelista Morais <thtmorais@hotmail.com>
2021-04-06 23:24:03 -0300

    Refactoring

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,7 @@
 ﻿#CHANGELOG(Registro de alterações)
 
+###[4.28.0] - Refactoring ([@thtmorais](https://gitlab.com/thtmorais))
+
 ###[1.0.0b] - Rename packages ([@thtmorais](https://gitlab.com/thtmorais))
 
 ###[1.0.0a] - LICENSE ([@thtmorais](https://gitlab.com/thtmorais))
--- src/FeatherIconsAsset.php
+++ src/FeatherIconsAsset.php
@@ -4,19 +4,28 @@ namespace thtmorais\icons;
 
 use yii\web\AssetBundle;
 
+/**
+ * Class FeatherIconsAsset
+ * @package thtmorais\icons
+ */
 class FeatherIconsAsset extends AssetBundle
 {
+    /**
+     * @var string
+     */
     public $sourcePath = "@npm";
 
+    /**
+     * @var string
+     */
     private $path = "feather-icons/dist/";
 
+    /**
+     * @param \yii\web\View $view
+     */
     public function registerAssetFiles($view)
     {
-        if(YII_DEBUG){
-            $this->js = [$this->path.'feather.js'];
-        }else{
-            $this->js = [$this->path.'feather.min.js'];
-        }
+        $this->js = [$this->path . YII_DEBUG ? 'feather.js' : 'feather.min.js'];
 
         parent::registerAssetFiles($view);
     }
