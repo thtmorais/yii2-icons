6ec88e17d067a797e111f16dd6a0387e71f609da
Matheus Evangelista Morais <thtmorais@hotmail.com>
2021-06-10 23:47:29 -0300

    FeatherIconsAsset
    Adjustment in FeatherIconsAsset from error when using dmstr/php-yii2:latest-nginx image
    
     ERROR: PHP Warning – yii\base\ErrorException  | copy(/app/vendor/npm-asset/classnames/bind.js): failed to open stream: Permission denied

--- src/FeatherIconsAsset.php
+++ src/FeatherIconsAsset.php
@@ -13,24 +13,12 @@ class FeatherIconsAsset extends AssetBundle
     /**
      * @var string
      */
-    public $sourcePath = "@npm";
-
+    public $sourcePath = "@npm/feather-icons/dist/";
+    
     /**
-     * @var string
+     * @var string[]
      */
-    private $path = "feather-icons/dist/";
-
-    /**
-     * @param \yii\web\View $view
-     */
-    public function registerAssetFiles($view)
-    {
-        if(YII_DEBUG){
-            $this->js = [$this->path.'feather.js'];
-        }else{
-            $this->js = [$this->path.'feather.min.js'];
-        }
-
-        parent::registerAssetFiles($view);
-    }
+    public $js = [
+        YII_DEBUG ? 'feather.js' : 'feather.min.js'
+    ];
 }
